d36ae74d5df479865fdb9a6c10a4fa5ef55b5feb
canans <serpercanan@gmail.com>
2021-05-11 02:17:11 +0300

    first commit

---
+++ src/main/java/com/example/MoviesComments/MoviesCommentsApplication.java
@@ -0,0 +1,13 @@
+package com.example.MoviesComments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesCommentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesCommentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MoviesComments/MoviesCommentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MoviesComments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesCommentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
